1739ce9f4e3a97bd284489a047d509148409e390
phiwa-tshimologong <phiwa@dsa.tshimologong.joburg>
2022-03-10 11:31:44 +0200

    Add styling on scroll

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 const nav = document.getElementById('navParent');
 
 if(window.location.pathname === '/index.html'){
-    nav.classList.add('nav-background');
+    window.addEventListener('scroll', () =>{
+        if(window.pageYOffset >= 200){
+            nav.classList.add('nav-background')
+            nav.classList.remove('nav-transparent')
+        } else{
+            nav.classList.add('nav-transparent')
+            nav.classList.remove('nav-background')
+        }
+    })
 }else{
-    nav.classList.remove('nav-parent');
+    nav.classList.remove('nav-background');
 }
+
+
+
+// window.onscroll = () => {
+//     console.log(window);
+// }
